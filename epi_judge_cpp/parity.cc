f982f112955dc42e6eefa80b1dc2864ceb9a8f2a
Sebastian Krämer <sebkraemer@posteo.org>
2023-08-31 17:48:37 +0000

    parity: use lookup table for parity computation

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -2,6 +2,11 @@
 #include <iostream>
 #include "test_framework/generic_test.h"
 
+constexpr auto bucket_size = 1 << 16; // space for 16 bit words
+static_assert(bucket_size == 65536);
+std::array<short, bucket_size> lut; // making this consteval would be neat
+
+
 template <typename T>
 void printBitRepresentation(T num) {
     std::bitset<sizeof(T) * 8> bits(num);
@@ -9,7 +14,7 @@ void printBitRepresentation(T num) {
 }
 
 short Parity(unsigned long long x) {
-  printBitRepresentation(x);
+  //printBitRepresentation(x);
 
   short result = 0;
   while (x) {
@@ -21,9 +26,24 @@ short Parity(unsigned long long x) {
   return result;
 }
 
+// splits parity calculation of 64 bit word into 4x16 bit words lookup
+short ParityLUT(unsigned long long x) {
+  short result = 0;
+  constexpr auto maskSize = 16;
+  constexpr auto mask = 0xFFFF;
+  result += lut[(x >> 0 * maskSize) & mask];
+  result += lut[(x >> 1 * maskSize) & mask];
+  result += lut[(x >> 2 * maskSize) & mask];
+  result += lut[(x >> 3 * maskSize) & mask];
+  return result % 2;
+}
+
 int main(int argc, char* argv[]) {
+  for(auto i = 0; i < lut.size(); i++) {
+    lut[i] = Parity(i); // initialize lookup table
+  }
+
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x"};
-  return GenericTestMain(args, "parity.cc", "parity.tsv", &Parity,
-                         DefaultComparator{}, param_names);
+  return GenericTestMain(args, "parity.cc", "parity.tsv", &ParityLUT, DefaultComparator{}, param_names);
 }
